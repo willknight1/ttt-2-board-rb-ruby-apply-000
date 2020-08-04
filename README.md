8c21c3cb8e0ea9ece8999528002e64d431bf151d
William Knight <wknight1027@gmail.com>
2020-08-04 20:34:08 +0000

    Done.

--- README.md
+++ README.md
@@ -98,3 +98,4 @@ row #=> [" ", "X", " "]
 5. Submit your solution with `learn submit`.
 
 <p data-visibility='hidden'>View <a href='https://learn.co/lessons/ttt-2-board-rb' title='Tic Tac Toe Board'>Tic Tac Toe Board</a> on Learn.co and start learning to code for free.</p>
+"hello"
--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,21 @@
-# Define the variable board below.
+board = Array
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+row = [" ", " ", " "]
+# position 1: Left 
+row[0]
+# position 2: Middle
+row[1]
+# position 3: Right
+row[2]
+# position 4: Left
+row[3]
+# position 5: Middle
+row[4]
+# position 6: Right
+row[5]
+# position 7: Left
+row[6]
+# position 8: Middle
+row[7]
+# position 9: Right
+row[8]
